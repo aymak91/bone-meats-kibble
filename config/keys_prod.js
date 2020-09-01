beb65e28587c63401f8f3ef23dcf1a34b420159d
Jordan Phan <JordanJPhan@gmail.com>
2020-09-01 14:13:35 -0700

    semi complete mapBox api

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,6 +2,6 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
     // where does the ending variable come from...
-    googleMapApi: process.env.GOOGLE_MAP_API
-
+    googleMapApi: process.env.GOOGLE_M,
+    mapBoxToken: process.env.MAP_BOX_TOKEN
 };
