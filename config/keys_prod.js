3b2edbf4d55c49afd878486d06cab6f5ae091aba
Jordan Phan <JordanJPhan@gmail.com>
2020-09-01 10:49:18 -0700

    update google map api key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
+    // where does the ending variable come from...
+    googleMapApi: process.env.GOOGLE_MAP_API
+
 };
