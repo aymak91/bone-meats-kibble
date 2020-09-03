05229560dbf2c5a76ce9b2dc312f4f0091f2e4d5
amakyu <amakyu@gmail.com>
2020-09-02 22:09:58 -0700

    Prep for heroku

---
+++ config/keys_dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://dev:REUfJO5bCfLXiCet@cluster0.wantb.mongodb.net/BoneMeatsKibble?retryWrites=true&w=majority",
+    //Make sure this is your own unique string
+    secretOrKey: "getboned",
+    awsAccessID: "AKIATLY5P3RCE42YQ5O6",
+    awsSecretAccessKey: "Lw7hBl0HQPOi4LT4cxLuoAdDRJ1JbpopxFt2XvDx",
+    s3Bucket: "bonemeatskibble-pro"
+};
