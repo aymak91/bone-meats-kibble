922e1104d4d8abe224a4a9c6f9d780f88e5c877d
Jordan Phan <JordanJPhan@gmail.com>
2020-09-06 18:03:18 -0700

    additional css for possible matches

--- frontend/src/components/search/search_index.js
+++ frontend/src/components/search/search_index.js
@@ -91,127 +91,145 @@ class SearchIndex extends React.Component {
     }
       
       return (
-        <div>
-          <div>
-            <select
-              className="gender"
-              value={this.state.gender}
-              onChange={this.switchOptions("gender")}
-            >
-              <option value=""> - </option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-            <select
-              className="breed"
-              value={this.state.breed}
-              onChange={this.switchOptions("breed")}
-            >
-              <option value=""> - </option>
-              <option value="Akita">Akita</option>
-              <option value="Alaskan Malamute">Alaskan Malamute</option>
-              <option value="American Bulldog">American Bulldog</option>
-              <option value="American Pitbull">American Pitbull</option>
-              <option value="Austrialian Sheperd">Austrialian Shepherd</option>
-              <option value="Beagle">Beagle</option>
-              <option value="Border Collie">Border Collie</option>
-              <option value="Boxer">Boxer</option>
-              <option value="Bulldog">Bulldog</option>
-              <option value="Cavalier King Charles Spaniel">Cavalier King Charles Spaniel</option>
-              <option value="Chihuahua">Chihuahua</option>
-              <option value="Chow Chow">Chow Chow</option>
-              <option value="Collie">Collie</option>
-              <option value="Dachshund">Dachshund</option>
-              <option value="Dalmatian">Dalmatian</option>
-              <option value="Doberman">Doberman</option>
-              <option value="English Setter">English Setter</option>
-              <option value="Doberman">Doberman</option>
-              <option value="French Bulldog">French Bulldog</option>
-              <option value="Doberman">Doberman</option>
-              <option value="German Shepard">German Shepard</option>
-              <option value="Doberman">Doberman</option>
-              <option value="Golden Retriever">Golden Retriever</option>
-              <option value="Great Dane">Great Dane</option>
-              <option value="Greyhound">Greyhound</option>
-              <option value="Jack Russel Terrier">Jack Russel Terrier</option>
-              <option value="Labrador Retriever">Labrador Retriever</option>
-              <option value="Maltese">Maltese</option>
-              <option value="maltese poodle">maltese poodle</option>
-              <option value="Mutt">Mutt</option>
-              <option value="Papillion">Papillion</option>
-              <option value="Pomeranian">Pomeranian</option>
-              <option value="Pomsky">Pomsky</option>
-              <option value="Poodle">Poodle</option>
-              <option value="Pug">Pug</option>
-              <option value="Rottweiler">Rottweiler</option>
-              <option value="Saint Bernard">Saint Bernard</option>
-              <option value="Samoyed">Samoyed</option>
-              <option value="Sottish Terrier">Scottish Terrier</option>
-              <option value="Shiba Inu">Shiba Inu</option>
-              <option value="Shih Tzu">Shih Tzu</option>
-              <option value="Siberian Husky">Siberian Husky</option>
-            </select>
-            <select
-              className="size"
-              value={this.state.size}
-              onChange={this.switchOptions("size")}
-            >
-              <option value=""> - </option>
-              <option value="Smol"> smol </option>
-              <option value="Small"> small </option>
-              <option value="Median"> Median </option>
-              <option value="Large"> Large </option>
-              <option value="Big Boi"> Big Boi </option>
-            </select>
-            <select
-              className="activeness"
-              value={this.state.activeness}
-              onChange={this.switchOptions("activeness")}
-            >
-              <option value=""> - </option>
-              <option value="Lazy">Lazy</option>
-              <option value="low">Low</option>
-              <option value="Normal"> Normal </option>
-              <option value="High"> High </option>
-              <option value="Hyperactive"> Hyperactive</option>
-            </select>
-            <select
-              className="personality"
-              value={this.state.personality}
-              onChange={this.switchOptions("personality")}
-            >
-              <option value=""> - </option>
-              <option value="Lonely"> Lonely </option>
-              <option value="Brave"> Brave </option>
-              <option value="Adamant"> Adamant </option>
-              <option value="bashful"> Bashful </option>
-              <option value="Naughty"> Naughty </option>
-              <option value="Bold"> Bold </option>
-              <option value="Relaxed"> Relaxed </option>
-              <option value="Impish"> Impish </option>
-              <option value="Lax"> Lax </option>
-              <option value="Timid"> Timid </option>
-              <option value="Hasty"> Hasty </option>
-              <option value="Jolly"> Jolly </option>
-              <option value="Naive"> Naive </option>
-              <option value="Modest"> Modest </option>
-              <option value="Mild"> Mild </option>
-              <option value="Quiet"> Quiet </option>
-              <option value="Rash"> Rash </option>
-              <option value="Calm"> Calm </option>
-              <option value="Gentle"> Gentle </option>
-              <option value="Sassy"> Sassy </option>
-              <option value="Careful"> Careful </option>
-            </select>
+        <div className="search-bgd">
+          <div className="search-bar">
+            <div className="search-option-container">
+              <label className="select-label"> Gender </label>
+              <select
+                className="select"
+                value={this.state.gender}
+                onChange={this.switchOptions("gender")} >
+                <option value=""> - </option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </div>
+            <div className="search-option-container">
+              <label className="select-label"> Breed </label>
+              <select
+                className="select"
+                value={this.state.breed}
+                onChange={this.switchOptions("breed")}
+              >
+                <option value=""> - </option>
+                <option value="Akita">Akita</option>
+                <option value="Alaskan Malamute">Alaskan Malamute</option>
+                <option value="American Bulldog">American Bulldog</option>
+                <option value="American Pitbull">American Pitbull</option>
+                <option value="American Staffordshire Terrier">American Staffordshire Terrier</option>
+                <option value="Austrialian Sheperd">Austrialian Shepherd</option>
+                <option value="Beagle">Beagle</option>
+                <option value="Blue Heeler Mix">Blue Heeler Mix</option>
+                <option value="Border Collie">Border Collie</option>
+                <option value="Boxer">Boxer</option>
+                <option value="Bulldog">Bulldog</option>
+                <option value="Cavalier King Charles Spaniel">Cavalier King Charles Spaniel</option>
+                <option value="Chihuahua">Chihuahua</option>
+                <option value="Chow Chow">Chow Chow</option>
+                <option value="Corgi">Corgi</option>
+                <option value="Collie">Collie</option>
+                <option value="Dachshund">Dachshund</option>
+                <option value="Dalmatian">Dalmatian</option>
+                <option value="Doberman">Doberman</option>
+                <option value="English Setter">English Setter</option>
+                <option value="Doberman">Doberman</option>
+                <option value="French Bulldog">French Bulldog</option>
+                <option value="Doberman">Doberman</option>
+                <option value="German Shepard">German Shepard</option>
+                <option value="Doberman">Doberman</option>
+                <option value="Golden Retriever">Golden Retriever</option>
+                <option value="Great Dane">Great Dane</option>
+                <option value="Greyhound">Greyhound</option>
+                <option value="Jack Russel Terrier">Jack Russel Terrier</option>
+                <option value="Labrador Retriever">Labrador Retriever</option>
+                <option value="Maltese">Maltese</option>
+                <option value="maltese poodle">maltese poodle</option>
+                <option value="Mutt">Mutt</option>
+                <option value="Papillion">Papillion</option>
+                <option value="Persian Yellow Mongrel">Persian Yellow Mongrel</option>
+                <option value="Pomeranian">Pomeranian</option>
+                <option value="Pomsky">Pomsky</option>
+                <option value="Poodle">Poodle</option>
+                <option value="Pug">Pug</option>
+                <option value="Rottweiler">Rottweiler</option>
+                <option value="Saint Bernard">Saint Bernard</option>
+                <option value="Samoyed">Samoyed</option>
+                <option value="Sottish Terrier">Scottish Terrier</option>
+                <option value="Shiba Inu">Shiba Inu</option>
+                <option value="Shih Tzu">Shih Tzu</option>
+                <option value="Siberian Husky">Siberian Husky</option>
+              </select>
+            </div>
+            <div className="search-option-container">
+              <label className="select-label"> Size </label>
+              <select
+                className="select"
+                value={this.state.size}
+                onChange={this.switchOptions("size")}
+              >
+                <option value=""> - </option>
+                <option value="Smol"> Smol </option>
+                <option value="Small"> Small </option>
+                <option value="Medium"> Medium </option>
+                <option value="Large"> Large </option>
+                <option value="Big Boi"> Big Boi </option>
+              </select>
+            </div>
+            <div className="search-option-container">
+              <label className="select-label"> Activeness </label>
+              <select
+                className="select"
+                value={this.state.activeness}
+                onChange={this.switchOptions("activeness")}
+              >
+                <option value=""> - </option>
+                <option value="Lazy">Lazy</option>
+                <option value="low">Low</option>
+                <option value="Normal"> Normal </option>
+                <option value="High"> High </option>
+                <option value="Hyperactive"> Hyperactive</option>
+              </select>
+            </div>
+            <div className="search-option-container">
+              <label className="select-label"> Personality </label>
+              <select
+                className="select"
+                value={this.state.personality}
+                onChange={this.switchOptions("personality")}
+              >
+                <option value=""> - </option>
+                <option value="Lonely"> Lonely </option>
+                <option value="Brave"> Brave </option>
+                <option value="Adamant"> Adamant </option>
+                <option value="Bashful"> Bashful </option>
+                <option value="Naughty"> Naughty </option>
+                <option value="Bold"> Bold </option>
+                <option value="Relaxed"> Relaxed </option>
+                <option value="Impish"> Impish </option>
+                <option value="Lax"> Lax </option>
+                <option value="Timid"> Timid </option>
+                <option value="Hasty"> Hasty </option>
+                <option value="Jolly"> Jolly </option>
+                <option value="Naive"> Naive </option>
+                <option value="Modest"> Modest </option>
+                <option value="Mild"> Mild </option>
+                <option value="Quiet"> Quiet </option>
+                <option value="Rash"> Rash </option>
+                <option value="Calm"> Calm </option>
+                <option value="Gentle"> Gentle </option>
+                <option value="Sassy"> Sassy </option>
+                <option value="Careful"> Careful </option>
+              </select>
+            </div>
           </div>
-          <ul>
+          <ul className="dog-index">
             {/* {this.state.compat_dogs.map((dog) => ( */}
             {filtered_personality.map((dog) => {
 
               return <SearchIndexItem dog={dog} key={dog.id} />;
             })}
           </ul>
-        </div>
+      </div>
       );
     }
 }
--- frontend/src/components/search/search_index_item.js
+++ frontend/src/components/search/search_index_item.js
@@ -1,21 +1,43 @@
 import React from 'react';
-
+import Moment from 'moment';
 
 const SearchIndexItem = props => {
 
     return (
-        <div>
-        <ul>
-            <li>{props.dog.breed}</li>
-            <li>{props.dog.description}</li>
-            <li>{props.dog.birthDate}</li>
-            <li>{props.dog.size}</li>
-            <li>{props.dog.gender}</li>
-            <li>{props.dog.activeness}</li>
-            <li>{props.dog.personality}</li>
+      <ul className="dog-index-item">
+        <li className="dog-name">
+          {props.dog.name}
+          <br />
+          Age: {Moment().diff(props.dog.birthDate, "years")}
+          <br />
+        </li>
+
+        <img className="match-image" src={`${props.dog.imageURL}`} />
+        <li className="dog-desc">
+          <div> Description: </div> <div> {props.dog.description} </div>
+        </li>
+        <div className="dog-details">
+          <li>
+            <span> Breed: </span>
             <br />
-        </ul>
+            <span> {props.dog.breed} </span>
+          </li>
+          <li>
+            <span> BDay: </span>{" "}
+            <span>{Moment(props.dog.birthDate).format("MMM Do YYYY")}</span>
+          </li>
+          <li>
+            <span>Size: </span> <span>{props.dog.size}</span>
+          </li>
+          <li>
+            <span>Gender: </span> <span>{props.dog.gender}</span>
+          </li>
+          <li>
+            <span>Activeness: </span> <span>{props.dog.activeness}</span>
+          </li>
         </div>
+        <br />
+      </ul>
     );
 }
 
